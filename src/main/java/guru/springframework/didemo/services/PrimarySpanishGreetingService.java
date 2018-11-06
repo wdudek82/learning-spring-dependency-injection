056413bf872dc48ca8802e48f264157825a7db7a
Wojtek Dudek <wdudek82@gmail.com>
2018-11-06 22:50:38 +0100

    Added multiple @Primary greeting services using @Profile(s)

--- src/main/java/guru/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile("es")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "=> PrimaryGreeingService.sayGreeting()";
+        return "Servicio de Saludo Primario";
     }
 }
